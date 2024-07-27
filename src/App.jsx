2696461fc267f3899e4c45935b549603f87154f7
flaviare1s <flaviarster@gmail.com>
2024-07-27 16:54:20 -0300

    Criei o componente de loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { HashRouter, Routes } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
 import '../src/styles/App.css'
 import { Footer } from './components/Footer'
+import { Loader } from './components/Loader'
 
 export const App = () => {
 
@@ -9,6 +10,7 @@ export const App = () => {
     <>
       <HashRouter>
         <Navbar />
+        <Loader />
         <main className='h-screen'>
           <Routes>
           
---
+++ src/components/Loader.jsx
@@ -0,0 +1,8 @@
+export const Loader = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-verde-oliva m-auto"></div>
+    </div>
+  )
+}
+
