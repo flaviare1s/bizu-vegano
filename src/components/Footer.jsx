746c7c635716d654b4c17aa9f46bbfae7d769bdb
flaviare1s <flaviarster@gmail.com>
2024-07-28 05:45:42 -0300

    Corrigi estilos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 export const Footer = () => {
   return (
     <footer className="bg-branco-quente text-verde-escuro text-center p-4">
-      <small>©️ 2024 Bizu Vegano - Desenvolvido por  
+      <small>©️ 2024 Bizu Vegano
+        <br /> 
+        Desenvolvido por  
         <a className="font-semibold underline ml-2 hover:font-bold" href="https://mail.google.com/mail/?view=cm&fs=1&to=flaviarster@gmail.com" target="_blank">Flávia Reis</a> - Fortaleza - CE
       </small>
     </footer>
