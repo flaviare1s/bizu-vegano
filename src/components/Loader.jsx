01d90455c82b1973eaf71fcd86cf7333347a5b21
flaviare1s <flaviarster@gmail.com>
2024-07-28 06:21:35 -0300

    Mudei a cor do loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 export const Loader = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-verde-oliva m-auto"></div>
+      <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-branco-quente m-auto"></div>
     </div>
   )
 }
