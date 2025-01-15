ff4df9070c20367aa7b98df25002df316596d61d
flaviare1s <flaviarster@gmail.com>
2025-01-15 10:47:50 -0300

    Update OutrosMunicipiosCearenses.jsx

--- src/components/OutrosMunicipiosCearenses.jsx
+++ src/components/OutrosMunicipiosCearenses.jsx
@@ -7,11 +7,13 @@ export const OutrosMunicipiosCearenses = () => {
       <ul className="list-disc ml-6">
         <li><a href="https://www.instagram.com/casaveg.eco?igsh=MTZ4NW55ZHMwN21kdg==">CasaVeg (100% veg) - Canoa Quebrada</a></li>
         <li><a href="https://www.instagram.com/recantonovavida_?igsh=ajQ1bXFraHkwbjFq">Recanto Nova Vida (100% veg) - Serra da Ibiapaba</a></li>
+        <li><a href="https://www.instagram.com/grand.vegano?igsh=MXhiZm1kN210ZnNraA==">Grand Vegano (100% veg) - Juazeiro do Norte</a></li>
         <li><a href="https://www.instagram.com/mezzaluna_canoa?igsh=OXo5M3h4aXc1cWV3">Mezza Luna - Cumbuco</a></li>
         <li><a href="https://www.instagram.com/allmassas?igsh=dXZrcDA4eDIyM2hy">All Massas - Aquiraz</a></li>
         <li><a href="https://www.instagram.com/korea.station.br?igsh=dmtxZHI3ZjdtajRy">Korea Station - Cumbuco</a></li>
         <li><a href="https://www.instagram.com/benzida_?igsh=MXg3Z2llYm45bDN2Zw==">Benzida - Canoa Quebrada</a></li>
         <li><a href="https://www.instagram.com/zorahbeachhotel?igsh=aHRkOTg4MGoyczc0">Zorah Beach Hotel - Trairi</a></li>
+        <li><a href="https://www.instagram.com/pizzaria_cogumelos?igsh=cjNtbDFrZTltMnQw">Pizzaria Cogumelos - Guaramiranga</a></li>
       </ul>
     </section>
   )
