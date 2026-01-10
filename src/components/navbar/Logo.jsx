f40b6f8bcc9eb51178fad1e8d50e7ae290574d89
flaviare1s <flaviarster@gmail.com>
2026-01-10 08:10:01 -0300

    Ajusta tamanho da fonte

--- src/components/navbar/Logo.jsx
+++ src/components/navbar/Logo.jsx
@@ -6,7 +6,7 @@ export const Logo = ({ closeMenu }) => {
   return (
     <Link to='/' className='h-[50px] sm:h-[65px] flex items-center gap-2' onClick={closeMenu}>
       <img className='h-full rounded-s-full' src={logo} alt="logo" />
-      <h1 className='font-logomarca text-2xl sm:text-3xl font-bold text-logo'>Bizu Vegano</h1>
+      <h1 className='font-logomarca text-3xl md:text-2xl lg:text-3xl font-bold text-logo'>Bizu Vegano</h1>
     </Link>
-  );
+  ); 
 };
