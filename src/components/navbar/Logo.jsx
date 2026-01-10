b295705332e7164efdeca1d1d63c8027166a5dec
flaviare1s <flaviarster@gmail.com>
2026-01-10 08:06:50 -0300

    Altera logo

--- src/components/navbar/Logo.jsx
+++ src/components/navbar/Logo.jsx
@@ -4,9 +4,9 @@ import logo from '../../assets/logo.png';
 
 export const Logo = ({ closeMenu }) => {
   return (
-    <Link to='/' className='h-[60px] sm:h-[75px] flex items-center gap-2' onClick={closeMenu}>
-      <img className='h-full rounded-s-full pr-1' src={logo} alt="logo" />
-      <h1 className='font-logomarca text-3xl md:text-2xl lg:text-3xl font-bold text-logo'>Bizu Vegano</h1>
+    <Link to='/' className='h-[50px] sm:h-[65px] flex items-center gap-2' onClick={closeMenu}>
+      <img className='h-full rounded-s-full' src={logo} alt="logo" />
+      <h1 className='font-logomarca text-2xl sm:text-3xl font-bold text-logo'>Bizu Vegano</h1>
     </Link>
   );
 };
